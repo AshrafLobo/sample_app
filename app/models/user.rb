9d2e538d91125e3ad0ff76b2b03858d1ea67996f
AshrafLobo <ashraflobo@gmail.com>
2017-12-15 14:36:36 +0300

    Format User Model Code

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   before_save { email.downcase! }
-  validates :name , presence: true, 
-                    length:   {maximum: 50}
+  validates :name, presence: true, 
+                   length:   {maximum: 50}
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence:   true, 
@@ -10,5 +10,6 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitive: false }
                     
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, 
+                       length: { minimum: 6 }
 end
