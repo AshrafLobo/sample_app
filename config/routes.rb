57b86c6d11c46b015222ba0beff5fd90af9f486b
AshrafLobo <ashraflobo@gmail.com>
2018-01-21 21:17:39 +0300

    Fix signup routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get  '/contact', to: 'static_pages#contact'
 
   #Users Controller
-  get '/signup', to: 'users#new'
+  get  '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
   resources :users
 end
